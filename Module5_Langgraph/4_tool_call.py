fb1ec95514fc19d7f40054ba83b68c540dd6cc90
shaktidharreddy <109575010+shaktidharreddy@users.noreply.github.com>
2026-09-26 18:35:33 +0530

    Implement stock price retrieval and chatbot logic

---
+++ Module5_Langgraph/4_tool_call.py
@@ -0,0 +1,65 @@
+from langchain.chat_models import init_chat_model
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain_core.tools import tool
+from langgraph.prebuilt import ToolNode, tools_condition
+
+from dotenv import load_dotenv
+load_dotenv()
+
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
+
+@tool
+def get_stock_price(symbol: str) -> float:
+    '''Return the current price of a stock given the stock symbol
+    :param symbol: stock symbol
+    :return: current price of the stock
+    '''
+    return {
+        "MSFT": 200.3,
+        "AAPL": 100.4,
+        "AMZN": 150.0,
+        "RIL": 87.6
+    }.get(symbol, 0.0)
+
+tools = [get_stock_price]
+
+llm = init_chat_model("google_genai:gemini-2.0-flash")
+llm_with_tools = llm.bind_tools(tools)
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+builder = StateGraph(State)
+
+builder.add_node(chatbot)
+builder.add_node("tools", ToolNode(tools))
+
+builder.add_edge(START, "chatbot")
+builder.add_conditional_edges("chatbot", tools_condition)
+
+graph = builder.compile()
+
+from IPython.display import Image, display
+
+display(Image(graph.get_graph().draw_mermaid_png()))
+
+state = graph.invoke({"messages": [{"role": "user", "content": "What is the price of AAPL stock right now?"}]})
+print(state["messages"][-1].content)
+
+
+state = graph.invoke({"messages": [{"role": "user", "content": "Who invented theory of relativity? print person name only"}]})
+print(state["messages"][-1].content)
+
+
+
+msg = "I want to buy 20 AMZN stocks using current price. Then 15 MSFT. What will be the total cost?"
+
+state = graph.invoke({"messages": [{"role": "user", "content": msg}]})
+print(state["messages"][-1].content)
