5a8f1b51aeb83641d4f076ce6fa275a1fea69a8a
shaktidharreddy <109575010+shaktidharreddy@users.noreply.github.com>
2026-09-26 18:30:52 +0530

    Add PortfolioState and conversion functions

---
+++ Module5_Langgraph/2_parallel_execution_without_reducer_but_List.py
@@ -0,0 +1,72 @@
+from typing import TypedDict, Literal
+from typing_extensions import Annotated
+from typing import List
+
+
+class PortfolioState(TypedDict):
+    amount_usd: float
+    total_usd: float
+    target_currency: Literal["INR", "EUR", "RUB"]
+    total: List[float]
+
+
+def calc_total(state: PortfolioState) -> PortfolioState:
+    state['total_usd'] = state['amount_usd'] * 1.08
+    return state
+
+def convert_to_inr(state: PortfolioState) -> dict:
+    return {"total": state["total"].append( [state["total_usd"] * 95])}
+
+def convert_to_eur(state: PortfolioState) -> dict:
+    return {"total": state["total"].append( [state["total_usd"] * 0.9])}
+
+def convert_to_rub(state: PortfolioState) -> dict:
+    return {"total": state["total"].append( [state["total_usd"] * 65])}
+
+def choose_conversion(state: PortfolioState) -> str:
+    return state["target_currency"]
+
+
+from langgraph.graph import StateGraph, START, END
+
+builder = StateGraph(PortfolioState)
+
+builder.add_node("calc_total_node", calc_total)
+builder.add_node("convert_to_inr_node", convert_to_inr)
+builder.add_node("convert_to_eur_node", convert_to_eur)
+builder.add_node("convert_to_rub_node", convert_to_rub)
+
+builder.add_edge(START, "calc_total_node")
+
+
+#parallel execution of all 3 nodes
+builder.add_edge("calc_total_node", "convert_to_inr_node")
+builder.add_edge("calc_total_node", "convert_to_eur_node")
+builder.add_edge("calc_total_node", "convert_to_rub_node")
+
+
+# conditional execution of all 3 nodes
+
+# builder.add_conditional_edges(
+#     "calc_total_node",
+#     choose_conversion,
+#     {
+#         "INR": "convert_to_inr_node",
+#         "EUR": "convert_to_eur_node",
+#         "RUB": "convert_to_rub_node",
+#     }
+# )
+
+builder.add_edge(["convert_to_inr_node", "convert_to_eur_node", "convert_to_rub_node"], END)
+
+graph = builder.compile()
+
+from IPython.display import Image, display
+
+display(Image(graph.get_graph().draw_mermaid_png()))
+
+
+graph.invoke({"amount_usd": 1000, "target_currency": "EUR"})
+
+
+# gives error - doesnt execute
